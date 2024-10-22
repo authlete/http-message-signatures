516a075d7177ea741e69b72fd0def2cc2a60c278
Takahiko Kawasaki <taka@authlete.com>
2024-10-22 18:42:40 +0900

    JavaDoc for version 1.3.

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.authlete.hms"},{"l":"com.authlete.hms.impl"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.authlete.hms"},{"l":"com.authlete.hms.fapi"},{"l":"com.authlete.hms.impl"}];updateSearchResults();
